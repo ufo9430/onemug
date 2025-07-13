6a75594c3bf41b58d8d67b10f7e7ad1efc1afc59
ufo9430 <ufo9430@naver.com>
2025-07-13 14:38:18 +0900

    choir: initialize Spring Boot project

---
+++ src/test/java/com/onemug/OnemugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onemug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnemugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
