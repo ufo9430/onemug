be7a8f12758089673cdb6bd4fa00e8b690d120e6
ufo9430 <ufo9430@naver.com>
2025-07-14 13:38:00 +0900

    fix: 엔티티 누락된 내용 추가

--- backend/src/main/java/com/onemug/global/entity/Comment.java
+++ backend/src/main/java/com/onemug/global/entity/Comment.java
@@ -1,12 +1,20 @@
 package com.onemug.global.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Document(collection = "comment")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Comment {
     @Id
     private ObjectId _id;
