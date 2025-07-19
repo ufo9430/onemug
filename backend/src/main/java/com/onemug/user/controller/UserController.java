8fe3154297ecc9a58535ca755296d9d5efff9f73
ufo9430 <ufo9430@naver.com>
2025-07-20 02:30:13 +0900

    feat: 사용자 프로필 조회 api 구현

--- backend/src/main/java/com/onemug/user/controller/UserController.java
+++ backend/src/main/java/com/onemug/user/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.onemug.user.controller;
 
 import com.onemug.global.entity.User;
+import com.onemug.user.model.CustomUserDetails;
+import com.onemug.user.service.CustomUserDetailsService;
 import com.onemug.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -15,10 +21,26 @@ public class UserController {
 
     private final UserService userService;
 
+    private final CustomUserDetailsService userDetailsService;
+
     // 회원 정보 조회
     @GetMapping("/{id}") 
     public Optional<User> findById(@PathVariable Long id) {
         return userService.findById(id);
+    }
+
+    //프로필 정보 리액트 전송용 api
+    @GetMapping("/profile")
+    public ResponseEntity<Map<String,Object>> getProfile(Authentication authentication){
+        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        Long userId = userDetails.getUser().getId();
+
+        Map<String, Object> profile = userDetailsService.getProfile(userId);
+
+        return ResponseEntity.ok(profile);
     }
 
     // 회원 정보 수정
